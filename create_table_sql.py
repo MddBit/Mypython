6d2a84b01903beb11b375db2f32a79970251b223
mdd <mdd23410@163.com>
2022-11-08 15:20:35 +0800

    1111

--- create_table_sql.py
+++ create_table_sql.py
@@ -28,40 +28,45 @@ def default_value(col_type):
         return 'error_check'
 
 
-path = '/Users/mdd/Documents/src/spyder/table_metadata/'
-table_metadatas = os.listdir(path)[1:]
+path1 = '/Users/mdd/Documents/src/Mypython/table_metadata/' # ods元数据
+path2 = '/Users/mdd/Documents/src/Mypython/all_sql_str.sql' # 结果输出，建表语句
+metadata_book_list = os.listdir(path1)
+metadata_book_list.remove('.DS_Store')
 
-all_sql_str = ''
-for table in table_metadatas:
-    
-    table_metadata = pd.read_excel(path + table)
 
-    col_min_index = 2
-    col_max_index = (table_metadata[table_metadata['name'] ==
-                            '# Detailed Table Information'].index.tolist()[0] - 1)
+f = open(path2, 'a')
+for book in metadata_book_list:
     
-    table_index = (table_metadata[table_metadata['type'] ==
-                            'kudu.table_name'].index.tolist()[0])
-    table_name = table_metadata['comment'][table_index]
+    sheet_list = pd.read_excel(path1 + book, sheet_name=None)
     
-    
-    sql_str = "CREATE TABLE mysql_source." + table_name +" (\n"
-    for i in range(col_min_index, col_max_index):
-        col_str = (table_metadata['name'][i] + " " + table_metadata['type'][i] +
-                       " not null default ")
-        default_val = default_value(table_metadata['type'][i])
-        col_str = col_str + default_val + " comment ''"
-        if i == col_min_index:
-            col_str = "    " + col_str + "\n"
-        else:
-            col_str = "    ," + col_str + "\n"
+    for sheet in sheet_list:
+        table_metadata = sheet_list[sheet]
         
-        sql_str += col_str
-    
-    sql_str += "    ,PRIMARY KEY (" + table_metadata['name'][col_min_index] + ')\n'
-    sql_str += ") PARTITION BY HASH (" + table_metadata['name'][col_min_index] + ')\n'
-    sql_str += "PARTITIONS 8 STORED AS KUDU TBLPROPERTIES ('kudu.master_addresses'='{KUDU_MASTER_ADDRESSES}')"
-    
-    all_sql_str = all_sql_str + sql_str + "\n\n"
-    
+        col_min_index = 2
+        col_max_index = table_metadata[table_metadata['name'] == '# Detailed Table Information'].index.tolist()[0] - 1
+        
+        table_index = table_metadata[table_metadata['type'] == 'kudu.table_name'].index.tolist()[0]
+        table_name = table_metadata['comment'][table_index]
+        
+        sql_str = "CREATE TABLE mysql_source." + table_name +" (\n"
+        for i in range(col_min_index, col_max_index):
+            col_str = (table_metadata['name'][i] + " " + table_metadata['type'][i] +
+                           " not null default ")
+            default_val = default_value(table_metadata['type'][i])
+            col_str = col_str + default_val + " comment ''"
+            if i == col_min_index:
+                col_str = "    " + col_str + "\n"
+            else:
+                col_str = "    ," + col_str + "\n"
+            
+            sql_str += col_str
+        
+        sql_str += "    ,PRIMARY KEY (" + table_metadata['name'][col_min_index] + ')\n'
+        sql_str += ") PARTITION BY HASH (" + table_metadata['name'][col_min_index] + ')\n'
+        sql_str += "PARTITIONS 8 STORED AS KUDU TBLPROPERTIES ('kudu.master_addresses'='{KUDU_MASTER_ADDRESSES}')"
+        
+        f.write(sql_str)
+        f.write("\n\n")
+f.close()
 
+    
--- dwd_used_ods.py
+++ dwd_used_ods.py
@@ -11,13 +11,15 @@ Created on Mon Nov  7 15:03:01 2022
 import re
 import pandas as pd
 
-path = '/Users/mdd/Documents/src/spyder/'
+path1 = "/Users/mdd/Documents/src/Mypython/etl_task.xlsx" # 当前在运行etl任务
+path2 = "/Users/mdd/Documents/src/realtime_dw_cloud/etl_task/" # etl任务xml文件
+path3 = "/Users/mdd/Documents/src/Mypython/dwd_used_ods.xlsx" # 结果表
 
-dwd_lst = pd.read_excel(path + 'etl_task.xlsx')
+dwd_lst = pd.read_excel(path1)
 
 dwd_ods = []
 for dwd in list(dwd_lst['etl_task_name']):
-    f = open(path + 'etl_task/' + dwd + '.xml')
+    f = open(path2 + dwd + '.xml')
     ods_lst = re.findall('\w+___\w+|flink_source\.\w+|gzlc_real\.\w+', f.read())
     f.close()
     
@@ -25,5 +27,5 @@ for dwd in list(dwd_lst['etl_task_name']):
         dwd_ods.append([dwd, ods, ods_lst.count(ods)]) # dwd任务，使用的ods表，使用次数
 
 dwd_ods = pd.DataFrame(dwd_ods, columns=['dwd_task_name', 'used_ods_table', 'used_cnt'])
-dwd_ods.to_excel('dwd_ods.xlsx', index=False)
+dwd_ods.to_excel(path3, index=False)
 
